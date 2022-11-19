3dd8d7b7d2da04e88debeb2c0063413879131e90
Adimac93 <adimac93@gmail.com>
2022-11-19 17:46:41 +0100

    Better auth response

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -8,20 +8,25 @@ export const actions: Actions = {
         let email = form.get("email");
         let password = form.get("password");
         if (!email || !password) return invalid(400, { info: "missing fields" });
-
+        if (!email) {
+        }
         email = email.toString();
         password = password.toString();
 
         let user = await db.user.findUnique({ where: { email } });
-        if (user) {
-            console.log(password);
-            let isVerified = await verify(user.password, password);
-            if (isVerified) {
-                let session = await db.session.create({
-                    data: { user: { connect: { id: user.id } } },
-                });
-                event.cookies.set("session", session.id);
-            }
+        if (!user) {
+            return invalid(400, { email, incorrect: true });
         }
+
+        let isVerified = await verify(user.password, password);
+        if (!isVerified) {
+            return invalid(400, { email, incorrect: true });
+        }
+
+        let session = await db.session.create({
+            data: { user: { connect: { id: user.id } } },
+        });
+
+        event.cookies.set("session", session.id);
     },
 };
--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -8,18 +8,18 @@ export const actions: Actions = {
         let email = form.get("email");
         let password = form.get("password");
         let rPassword = form.get("repeat-password");
-        if (!email || !password || !rPassword) return { info: "missing fields" };
+
+        if (!email || !password || !rPassword) {
+            return invalid(400, { email, missing: true });
+        }
+        if (password != rPassword) {
+            return invalid(400, { email, incorrect: true });
+        }
 
         email = email.toString();
         password = password.toString();
 
         password = await hash(password);
-        if (!email) {
-            return invalid(400, { email, missing: true });
-        }
-        if (!password) {
-            return invalid(400, { password, missing: true });
-        }
 
         let isAlreadyRegistered = await db.user.findUnique({ where: { email } });
         if (isAlreadyRegistered) return invalid(400, { info: "user already exists" });
@@ -27,7 +27,7 @@ export const actions: Actions = {
         let session = await db.session.create({
             data: { user: { create: { email, password, coins: 0, username: "Hackyouser" } } },
         });
-        console.log(session);
+
         event.cookies.set("session", session.id, { httpOnly: true, secure: true });
     },
 };
