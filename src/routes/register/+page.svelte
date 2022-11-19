06a91d169c17ef73df258ad2a9a60fcc631e15f9
Mabi19 <mateuszbartnik33@gmail.com>
2022-11-19 16:02:00 +0100

    Add registration form

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Button from "$lib/components/Button.svelte";
+</script>
+
+<form method="POST" class="form">
+    <label>E-mail <input name="email" type="email" required /></label>
+    <label>Password <input name="password" type="password" required /></label>
+    <label>Repeat password <input name="repeat-password" required /></label>
+    <Button type="primary">Register</Button>
+</form>
+
+<style lang="scss">
+    .form {
+        display: flex;
+        flex-flow: column nowrap;
+        gap: 6px;
+        width: 40ch;
+        margin: auto;
+
+        label {
+            display: flex;
+            flex-flow: column nowrap;
+        }
+    }
+</style>
