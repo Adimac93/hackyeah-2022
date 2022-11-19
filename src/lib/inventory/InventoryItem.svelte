9f038476a92d029af3ec28751b58266dd1511ee2
Mabi19 <mateuszbartnik33@gmail.com>
2022-11-19 15:37:26 +0100

    Add basic inventory infrastructure

---
+++ src/lib/inventory/InventoryItem.svelte
@@ -0,0 +1,27 @@
+<div class="inventory-slot">
+    {itemData.icon} {itemData.name}
+    {#if count}
+        <span class="count">&#xd7; {count}</span>
+    {/if}
+</div>
+
+<script lang="ts">
+    import { getItemData } from "./Item";
+    // TODO: make this an enum or something idk
+    export let id: string;
+    export let count: number = 0;
+    $: itemData = getItemData(id);
+</script>
+
+<style lang="scss">
+    .inventory-slot {
+        background-color: var(--bg-2);
+        padding: 6px 8px;
+        width: max-content;
+        border-radius: 4px;
+    }
+
+    .count {
+        color: #5f5f5f;
+    }
+</style>
---
+++ src/lib/inventory/Item.ts
@@ -0,0 +1,20 @@
+import rawData from "../../data/items.json";
+
+export interface Item {
+    name: string;
+    icon: string;
+}
+
+const data = rawData as Record<string, Item>;
+
+export function isValidItem(id: string) {
+    return id in data;
+}
+
+export function getItemData(id: string) {
+    if (!isValidItem(id)) {
+        throw Error(`Invalid item ID ${id}`);
+    }
+
+    return data[id] as Item;
+}
