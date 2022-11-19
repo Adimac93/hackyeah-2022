3b71227f1748079fbe817ded631d4b3bb203be8e
Vixen <infvix@gmail.com>
2022-11-19 20:24:00 +0100

    Add admin panel

--- src/routes/admin-panel/+page.server.ts
+++ src/routes/admin-panel/+page.server.ts
@@ -1,7 +1,24 @@
-import type { Actions } from "@sveltejs/kit";
+import { db } from "$lib/server/database";
+import { invalid, type Actions } from "@sveltejs/kit";
 
 export const actions: Actions = {
     default: async (event) => {
-        
+        let user = event.locals.user;
+        if (!user)
+            return invalid(401, {});
+
+        let userGroup = user.group;
+        if (!userGroup)
+            return invalid(400, {});
+        if (!userGroup.isOwner)
+            return invalid(400, {});
+
+        let form = await event.request.formData();
+        let email = form.get("email");
+
+        if (!email)
+            return invalid(400, { info: "missing fields" });
+
+        await db.groupInvite.create({ data: { group: userGroup.group, email: email } });
     },
 };
