67bbdd9ef4f0e626de120e90177875a85e535cb5
Vixen <infvix@gmail.com>
2022-11-19 22:19:59 +0100

    Add redirects

--- src/routes/group-select/+page.server.ts
+++ src/routes/group-select/+page.server.ts
@@ -1,10 +1,15 @@
 import { db } from "$lib/server/database";
+import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ locals }) => {
     let user = locals.user;
     if (!user)
         return { status: 401 };
+
+    let userGroup = await db.userGroup.findUnique({ where: { userId: user.id } });
+    if (userGroup)
+        throw redirect(302, "/");
 
     return {
         invites: (await db.groupInvite.findMany({ where: { email: user.email }})).map(x => x.groupId)
