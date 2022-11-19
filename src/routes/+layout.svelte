8d56b52534b564dc2533f5d0f7cd2f554546c40f
Mabi19 <mateuszbartnik33@gmail.com>
2022-11-19 11:57:10 +0100

    Add basic layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<nav>
+    This is a navbar
+</nav>
+
+<slot/>
+
+<footer class="footer">
+    This is a footer that additionally looks cool.
+</footer>
+
+<style>
+    .footer {
+        background-color: aquamarine;
+        padding: 8px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>This is a header</h1>
+<p>This is a paragraph, and <a href="https://example.com">this is a link</a></p>
