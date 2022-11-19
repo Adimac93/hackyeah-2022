eb85ecfed1cf087130a7cf19324e420f8eabf3eb
Adimac93 <adimac93@gmail.com>
2022-11-19 17:28:48 +0100

    Auth session

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,7 +1,27 @@
-import type { Actions } from "@sveltejs/kit";
+import { db } from "$lib/server/database";
+import { invalid, type Actions } from "@sveltejs/kit";
+import { verify } from "argon2";
 
 export const actions: Actions = {
     default: async (event) => {
-        // TODO: log the user in
+        let form = await event.request.formData();
+        let email = form.get("email");
+        let password = form.get("password");
+        if (!email || !password) return invalid(400, { info: "missing fields" });
+
+        email = email.toString();
+        password = password.toString();
+
+        let user = await db.user.findUnique({ where: { email } });
+        if (user) {
+            console.log(password);
+            let isVerified = await verify(user.password, password);
+            if (isVerified) {
+                let session = await db.session.create({
+                    data: { user: { connect: { id: user.id } } },
+                });
+                event.cookies.set("session", session.id);
+            }
+        }
     },
 };
--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,7 +1,33 @@
-import type { Actions } from "@sveltejs/kit";
+import { db } from "$lib/server/database";
+import { invalid, type Actions } from "@sveltejs/kit";
+import { hash } from "argon2";
 
 export const actions: Actions = {
     default: async (event) => {
-        // TODO: log the user in
+        let form = await event.request.formData();
+        let email = form.get("email");
+        let password = form.get("password");
+        let rPassword = form.get("repeat-password");
+        if (!email || !password || !rPassword) return { info: "missing fields" };
+
+        email = email.toString();
+        password = password.toString();
+
+        password = await hash(password);
+        if (!email) {
+            return invalid(400, { email, missing: true });
+        }
+        if (!password) {
+            return invalid(400, { password, missing: true });
+        }
+
+        let isAlreadyRegistered = await db.user.findUnique({ where: { email } });
+        if (isAlreadyRegistered) return invalid(400, { info: "user already exists" });
+
+        let session = await db.session.create({
+            data: { user: { create: { email, password, coins: 0, username: "Hackyouser" } } },
+        });
+        console.log(session);
+        event.cookies.set("session", session.id, { httpOnly: true, secure: true });
     },
 };
