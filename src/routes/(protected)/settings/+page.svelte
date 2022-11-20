089a4a8f42114dafff6fd10ffc4e952591c12a51
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2022-11-20 11:09:22 +0100

    merge profile into settings

--- src/routes/(protected)/settings/+page.svelte
+++ src/routes/(protected)/settings/+page.svelte
@@ -1,23 +1,49 @@
 <script lang="ts">
+    import type { PageData } from "./$types";
     import Button from "$lib/components/Button.svelte";
     import LinkButton from "$lib/components/LinkButton.svelte";
-    import type { PageData } from "./$types";
 
     export let data: PageData;
 </script>
 
-{#if data.isOwner}
-    <LinkButton type="primary" href="/admin-panel">Admin Panel</LinkButton>
-{:else}
+<div class="wrapper">
+    <img src="/giraffe.png" alt="Giraffe" />
+    <h1>{data.username}</h1>
+    <p>{data.email}</p>
+    <p>Coins: {data.coins}</p>
+    <p>Score: {data.score}</p>
+    {#if data.isOwner}
+        <LinkButton type="primary" href="/admin-panel">Admin Panel</LinkButton>
+    {:else}
+        <form method="POST" class="form">
+            <input type="hidden" name="type" value="leave" />
+
+            <Button type="destructive">Leave the group</Button>
+        </form>
+    {/if}
     <form method="POST" class="form">
-        <input type="hidden" name="type" value="leave" />
-    
-        <Button type="destructive">Leave a group</Button>
+        <input type="hidden" name="type" value="delete" />
+
+        <Button type="destructive">Delete account</Button>
     </form>
-{/if}
+</div>
 
-<form method="POST" class="form">
-    <input type="hidden" name="type" value="delete" />
-
-    <Button type="destructive">Delete account</Button>
-</form>
+<style>
+    :global(body) {
+        background-color: #eee;
+    }
+    .wrapper {
+        margin: 2rem auto;
+        padding: 3rem;
+        width: fit-content;
+        background-color: white;
+        border-radius: 1rem;
+        display: flex;
+        align-items: stretch;
+        flex-flow: column nowrap;
+        gap: 1rem;
+    }
+    .wrapper * {
+        margin: 0;
+    }
+</style>
