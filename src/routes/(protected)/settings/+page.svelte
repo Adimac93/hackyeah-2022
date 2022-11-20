d484510d0b5c271813863d6846056ac29c7a2b0c
Vixen <infvix@gmail.com>
2022-11-20 05:44:47 +0100

    Add settings

---
+++ src/routes/(protected)/settings/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import Button from "$lib/components/Button.svelte";
+    import LinkButton from "$lib/components/LinkButton.svelte";
+    import type { PageData } from "./$types";
+
+    export let data: PageData;
+</script>
+
+{#if data.isOwner}
+    <LinkButton type="primary" href="/admin-panel">Admin Panel</LinkButton>
+{:else}
+    <Button type="destructive">Leave a group</Button>
+{/if}
+
+<Button type="destructive">Remove account</Button>
