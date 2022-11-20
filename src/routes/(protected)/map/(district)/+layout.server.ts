9e71cf628c14fd9a96ba06d8d7707d8e7a89c3ff
Mabi19 <mateuszbartnik33@gmail.com>
2022-11-20 06:30:52 +0100

    stuff i guess

--- src/routes/(protected)/map/(district)/+layout.server.ts
+++ src/routes/(protected)/map/(district)/+layout.server.ts
@@ -2,9 +2,9 @@ import { db } from "$lib/server/database";
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async ({ locals }) => {
-    const coins = await db.user.findFirst({
+    const coinsPromise = db.user.findFirst({
         where: { id: locals.user!.id },
         select: { coins: true },
     });
-    return { coins: coins || 0 };
+    return { coins: (await coinsPromise)?.coins ?? 0 };
 };
