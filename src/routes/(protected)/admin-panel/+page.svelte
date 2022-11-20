be934f656bb6c2812a102e7fc52d618056f1d307
Vixen <infvix@gmail.com>
2022-11-20 06:01:26 +0100

    Add delete group button

--- src/routes/(protected)/admin-panel/+page.svelte
+++ src/routes/(protected)/admin-panel/+page.svelte
@@ -6,13 +6,16 @@
 </script>
 
 <form method="POST" class="form">
+    <input type="hidden" name="type" value="invite" />
+
     <label>E-mail <input name="email" type="email" required /></label>
     <Button type="primary">Add to group</Button>
 </form>
 
 {#each data.users as user}
     <form method="POST" class="form">
-        <input type="hidden" name="userId" value={user.user.id} />
+        <input type="hidden" name="type" value="kick" />
+        <input type="hidden" name="userId" value="{user.user.id}" />
 
         {user.user.username} ({user.user.email})
         {#if user.isOwner}
@@ -22,3 +25,8 @@
         {/if}
     </form>
 {/each}
+
+<form method="POST" class="form">
+    <input type="hidden" name="type" value="delete" />
+    <Button type="destructive">Delete group</Button>
+</form>
