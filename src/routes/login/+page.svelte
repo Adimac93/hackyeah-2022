4a4b0af50a45841b8a77b2e9061f021351f1b2ba
Mabi19 <mateuszbartnik33@gmail.com>
2022-11-20 11:21:48 +0100

    Fancy login and register screens

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,24 +5,42 @@
     export let form: ActionData;
 </script>
 
-<form method="POST" class="form">
-    <label>E-mail <input name="email" type="email" required /></label>
-    <label>Password <input name="password" type="password" required /></label>
-    <Button type="primary">Log in</Button>
-    {#if form?.incorrect}
-        <div>Incorrect credentials</div>
-    {/if}
-    <span>or</span>
-    <LinkButton href="/register" type="secondary">Register</LinkButton>
-</form>
+<div class="global-wrapper">
+    <form method="POST" class="form">
+        <h1>Log in</h1>
+        <label>E-mail <input name="email" type="email" required /></label>
+        <label>Password <input name="password" type="password" required /></label>
+        <Button type="primary">Log in</Button>
+        {#if form?.incorrect}
+            <div>Incorrect credentials</div>
+        {/if}
+        <span>or</span>
+        <LinkButton href="/register" type="secondary">Register</LinkButton>
+    </form>
+</div>
 
 <style lang="scss">
+    .global-wrapper {
+        width: 100%;
+        height: 100vh;
+        background: linear-gradient(0.17turn, rgba(59,129,136,1) 26%, rgba(9,121,98,1) 49%, rgba(7,136,125,1) 67%, rgba(0,212,255,1) 100%);
+        display: grid;
+        place-items: center;
+    }
+
+    h1 {
+        margin: 0;
+    }
+
     .form {
         display: flex;
         flex-flow: column nowrap;
         gap: 6px;
         width: 40ch;
         margin: auto;
+        background-color: white;
+        border-radius: 8px;
+        padding: 8px;
 
         label {
             display: flex;
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -5,25 +5,44 @@
     export let form: ActionData;
 </script>
 
-<form method="POST" class="form">
-    <label>E-mail <input name="email" type="email" required /></label>
-    <label>Password <input name="password" type="password" required /></label>
-    <label>Repeat password <input name="repeat-password" type="password" required /></label>
-    <Button type="primary">Register</Button>
-    {#if form?.info}
-        <div>{form.info}</div>
-    {/if}
-    <span>or</span>
-    <LinkButton href="/login" type="secondary">Login</LinkButton>
-</form>
+<div class="global-wrapper">
+    <form method="POST" class="form">
+        <h1>Register</h1>
+        <label>E-mail <input name="email" type="email" required /></label>
+        <label>Password <input name="password" type="password" required /></label>
+        <label>Repeat password <input name="repeat-password" type="password" required /></label>
+        <Button type="primary">Register</Button>
+        {#if form?.info}
+            <div>{form.info}</div>
+        {/if}
+        <span>or</span>
+        <LinkButton href="/login" type="secondary">Login</LinkButton>
+    </form>
+</div>
 
 <style lang="scss">
+    .global-wrapper {
+        width: 100%;
+        height: 100vh;
+        background: linear-gradient(0.44turn, #fec24f, #fe4999);
+        display: grid;
+        place-items: center;
+    }
+
+    h1 {
+        margin: 0;
+    }
+
     .form {
         display: flex;
         flex-flow: column nowrap;
         gap: 6px;
         width: 40ch;
         margin: auto;
+
+        background-color: white;
+        border-radius: 8px;
+        padding: 8px;
 
         label {
             display: flex;
