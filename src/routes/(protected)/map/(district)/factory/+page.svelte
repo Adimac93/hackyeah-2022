6bf3aadf154bf9302c0524e72c868c07f373a655
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2022-11-20 09:24:54 +0100

    add factory and office districts

---
+++ src/routes/(protected)/map/(district)/factory/+page.svelte
@@ -0,0 +1,10 @@
+<img src="/factory_district.png" alt="Factory District" class="background-image" />
+
+<style>
+    .background-image {
+        display: block;
+        margin: auto;
+        min-height: 100%;
+        aspect-ratio: 1190 / 567;
+    }
+</style>
---
+++ src/routes/(protected)/map/(district)/office/+page.svelte
@@ -0,0 +1,10 @@
+<img src="/office_district.png" alt="Office District" class="background-image" />
+
+<style>
+    .background-image {
+        display: block;
+        margin: auto;
+        min-height: 100%;
+        aspect-ratio: 1190 / 567;
+    }
+</style>
